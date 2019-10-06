c9050367df6e12e9967a1c413d30efedf5e26d58
Clevinz <36393523+Clevinz@users.noreply.github.com>
2019-10-06 20:00:06 +0200

    Added club class

--- Minigolf/Minigolf/Player.h
+++ Minigolf/Minigolf/Player.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "DynamicObject.h"
+#include "Club.h"
+
 #include <Keyboard.h>
 #include <GamePad.h>
 
 class Player : public DynamicObject
 {
+private:
+	Club _club;
+
 public:
 	Player(ID3D11Device* device, ID3D11DeviceContext* deviceContext, BoundingType boundingType, DirectX::XMVECTOR startingPosition, std::string modelFile);
 	virtual ~Player();
